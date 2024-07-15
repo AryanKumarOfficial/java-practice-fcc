f7af16cff4e258dadbbd869e3610a089f3803343
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-15 12:33:22 +0530

    variables and datatypes

--- src/com/example/androidDev/Main.java
+++ src/com/example/androidDev/Main.java
@@ -3,5 +3,25 @@ package com.example.androidDev;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int a = 51;
+        System.out.println(a);
+
+        long b = 51;
+        System.out.println(b);
+
+        float c = (float) 4.5;
+        System.out.println(c);
+
+        double d = 4.596;
+        System.out.println(d);
+
+        char e = '\u00AE'; // ®
+        System.out.println(e);
+
+        String str = "string";
+        System.out.println(str);
+
+        boolean f = true;
+        System.out.println(!f);
     }
 }
